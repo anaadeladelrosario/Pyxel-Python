c795f36ca67657f46adadc0d7c1feec681f984a8
Ana <anaadeladelrosario@gmail.com>
2025-02-23 19:04:00 +0100

    fixing states

--- claw.py
+++ claw.py
@@ -42,14 +42,14 @@ class Claw:
     def __init__(self):
         self.x = 64
         self.y = 20
-        # self.arm_length_x = 48
-        # self.arm_length_y = 8
 
         self.arm_length_min=8
-        self.arm_length_max=50
+        self.arm_length_max=40
 
         self.arm_length = self.arm_length_min
         self.state = self.State.DEFAULT
+
+        self.grab_timer = 0
 
     def draw(self, debug = False):
 
@@ -60,21 +60,15 @@ class Claw:
         wheel_y = 24 + 8*((self.x // 8) % 2)
         pyxel.blt(self.x, 20, 0, wheel_x, wheel_y, 8, 8, 14)
 
-      
-            
-        # blt(x, y, img, u, v, w, h, [colkey], [rotate], [scale])
-        # pyxel.blt(self.x -8, self.line-8,0, self.arm_length_x, self.arm_length_y,16,8, 14) 
-
         self.draw_arm()
         self.draw_claw()
 
         if(debug):
             pyxel.text(4, 4, f"X: {self.x} Y: {self.y}", 0)
-            ## line(x1,y1,x2,y2,color)
+
             pyxel.line(0, self.y, 140, self.y, 3)
             pyxel.line(self.x, 0, self.x, 120, 7)
             
-            # pyxel.line(0, self.leng, 140, self.line, 3) ## arm starting point
             pyxel.text(4, 10, f"State: {self.state}", 0)
             pyxel.text(4, 16, f"Arm Length: {self.arm_length}", 0)
 
@@ -84,9 +78,9 @@ class Claw:
 
     def draw_arm(self):
         start_y=self.y+12
-        drawLine_y(start_y)
+        # drawLine_y(start_y)
         end_y=self.arm_length+start_y
-        drawLine_y(end_y,9)
+        # drawLine_y(end_y,9)
         arm_x=self.x -8
         arm_y=self.arm_length-8
         
@@ -95,16 +89,21 @@ class Claw:
 
     def draw_claw(self):
         claw_y = self.y +12
-        pyxel.blt(self.x-8, claw_y + self.arm_length,0,32,0, 16, 16, 14)
+        if self.state == Claw.State.GRABBING:    
+            pyxel.blt(self.x-8, claw_y + self.arm_length,0,32,0, 16, 16, 14)
+        else:
+            pyxel.blt(self.x-8, claw_y + self.arm_length,0,32,16, 16, 16, 14)
 
     def update(self):  
 
-        if self.state == self.State.DEFAULT:
+        if self.state == Claw.State.DEFAULT:
             self.update_default()
-        elif self.state == self.State.DESCENDING:
+        elif self.state == Claw.State.DESCENDING:
             self.update_descending()
-        elif self.state == self.State.GRABBING:
+        elif self.state == Claw.State.GRABBING:
             self.update_grabbing()
+        elif self.state == Claw.State.ASCENDING:
+            self.update_ascending()
 
         if pyxel.btn(pyxel.KEY_DOWN):
             self.arm_length = min(self.arm_length + 2, self.arm_length_max)              
@@ -117,15 +116,23 @@ class Claw:
         if pyxel.btn(pyxel.KEY_RIGHT):
             self.x = min(self.x + 2, pyxel.width - 16)   
         if pyxel.btn(pyxel.KEY_SPACE):
-            self.state = self.State.DESCENDING   
+            self.state = Claw.State.DESCENDING   
 
     def update_descending(self):    
-        self.arm_length += 1
+        self.arm_length += 1 
         if self.arm_length > self.arm_length_max:
-            self.state = self.State.GRABBING
+            self.grab_timer = 15
+            self.state = Claw.State.GRABBING
 
     def update_grabbing(self):
-        pass
+        self.grab_timer -= 1
+        if self.grab_timer == 0:
+            self.state = Claw.State.ASCENDING
+
+    def update_ascending(self):
+        self.arm_length -=1
+        if self.arm_length == self.arm_length_min:
+            self.state = Claw.State.DEFAULT
 
 def drawLine_y(y, col = 8):
     pyxel.line(0, y, 128, y, col)
