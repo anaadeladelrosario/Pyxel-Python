01cabf1850019f08ecf254a941441532ccc8aa1d
Ana <anaadeladelrosario@gmail.com>
2025-02-24 21:38:02 +0100

    constants

--- claw.py
+++ claw.py
@@ -1,6 +1,7 @@
 import pyxel
 from enum import Enum
-
+SCREEN_WIDTH = 128
+SCREEN_HEIGHT = 120
 
 class App:
     class State(Enum):
@@ -8,14 +9,16 @@ class App:
         DISPLAYING_MESSAGE = 1
 
     def __init__(self):
-        pyxel.init(128, 120, title="Motivational Claw Game")
+        pyxel.init(SCREEN_WIDTH, SCREEN_HEIGHT, title="Motivational Claw Game")
         pyxel.load("claw.pyxres")
-        self.claw = Claw()
-        self.state = App.State.GAME
-
+        self.init()
         pyxel.playm(0, loop=True)
         pyxel.run(self.update, self.draw)
-        
+
+    def init(self):
+        self.claw = Claw()
+        self.state = App.State.GAME
+  
 
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
@@ -23,10 +26,11 @@ class App:
 
         if self.state == App.State.GAME:            
             self.claw.update()
+            if self.claw.state == Claw.State.FINISHED:
+                self.state = App.State.DISPLAYING_MESSAGE
         elif self.state == App.State.DISPLAYING_MESSAGE:
             pass
-        
-        
+            
 
     def draw(self):
         if self.state == App.State.GAME:
@@ -36,16 +40,27 @@ class App:
 
     def draw_game(self):
         # backdrop
-        pyxel.bltm(0, 30, 0, 0, 30, 128, 78)
+        pyxel.bltm(0, 30, 0, 0, 30, SCREEN_WIDTH, 78)
             # # machine overlay
-        pyxel.bltm(0, 0, 0, 0, 0, 128, 30)
-
+        pyxel.bltm(0, 0, 0, 0, 0, SCREEN_WIDTH, 30)
+        self.draw_background_messages()
         self.claw.draw(True)
-        pyxel.bltm(0, 108, 0, 0, 108, 128, 20)
+        pyxel.bltm(0, 108, 0, 0, 108, SCREEN_WIDTH, 20)
 
     def draw_message(self):
-        pyxel.bltm(0,0,1,0,0,128,120)
+        pyxel.bltm(0,0,1,0,0,SCREEN_WIDTH,SCREEN_HEIGHT)
 
+    def draw_background_messages(self):
+        def draw_scroll(x, height, col):
+            floor = 108
+            pyxel.rect(x, floor-height, 2, height, col)
+        counter = 0
+        # while counter < SCREEN_WIDTH
+        #   draw a rolled up message at the counter X position at the bottom of the prize machine
+        #   add a random number to the counter
+        draw_scroll(6, 10,13)
+
+        pass
 
 class Claw:
     class State(Enum):
@@ -53,19 +68,19 @@ class Claw:
         DESCENDING = 3
         GRABBING = 4
         ASCENDING = 5
-        DYSPLAYING_MESSAGE = 6
+        FINISHED = 6
     def __init__(self):
         self.x = 64
         self.y = 20
 
         self.arm_length_min=8
-        self.arm_length_max=40 
+        self.arm_length_max=60 
 
         self.arm_length = self.arm_length_min
         self.state = self.State.DEFAULT
 
         self.grab_timer = 0
-
+    
 
     def draw(self, debug = False):
 
@@ -82,8 +97,8 @@ class Claw:
         if(debug):
             pyxel.text(4, 4, f"X: {self.x} Y: {self.y}", 0)
 
-            pyxel.line(0, self.y, 140, self.y, 3)
-            pyxel.line(self.x, 0, self.x, 120, 7)
+            # pyxel.line(0, self.y, 140, self.y, 3)
+            # pyxel.line(self.x, 0, self.x, 120, 7)
             
             pyxel.text(4, 10, f"State: {self.state}", 0)
             pyxel.text(4, 16, f"Arm Length: {self.arm_length}", 0)
@@ -100,7 +115,7 @@ class Claw:
         arm_x=self.x -8
         arm_y=self.arm_length-8
         
-        # pyxel.blt(arm_x, arm_y,0, 48, 8,16,8, 14) 
+        # pyxel.blt(arm_x, arm_y,0, 48, 8,16,8, 14)   
         if self.state == Claw.State.DEFAULT:
          pyxel.blt(self.x-8, 32,0,48,8, 16, 8, 14)
         elif self.state == Claw.State.DESCENDING or Claw.State.GRABBING:
@@ -114,7 +129,7 @@ class Claw:
     def draw_claw(self):
         claw_y = self.y +12
         if self.state == Claw.State.GRABBING or self.state == Claw.State.ASCENDING:    
-            pyxel.blt(self.x-8, claw_y + self.arm_length,0,32,0, 16, 16, 14)
+            pyxel.blt(self.x-8, claw_y + self.arm_length,0,32,32, 16, 24, 14)
         else:
             pyxel.blt(self.x-8, claw_y + self.arm_length,0,32,16, 16, 16, 14)
 
@@ -156,9 +171,9 @@ class Claw:
     def update_ascending(self):
         self.arm_length -=1
         if self.arm_length == self.arm_length_min:
-            self.state = Claw.State.DEFAULT
+            self.state = Claw.State.FINISHED
 
 def drawLine_y(y, col = 8):
-    pyxel.line(0, y, 128, y, col)
+    pyxel.line(0, y, SCREEN_WIDTH, y, col)
 
 App()
