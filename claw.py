5f5978888504122e87be92ee5e09cd0941d4d49d
Ana <anaadeladelrosario@gmail.com>
2025-02-25 13:38:03 +0100

    wip

--- claw.py
+++ claw.py
@@ -1,4 +1,5 @@
 import pyxel
+import random
 from enum import Enum
 SCREEN_WIDTH = 128
 SCREEN_HEIGHT = 120
@@ -18,19 +19,17 @@ class App:
     def init(self):
         self.claw = Claw()
         self.state = App.State.GAME
-  
+        self.message = Message()
 
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
-
-        if self.state == App.State.GAME:            
+        if self.state == App.State.GAME:         
             self.claw.update()
             if self.claw.state == Claw.State.FINISHED:
                 self.state = App.State.DISPLAYING_MESSAGE
         elif self.state == App.State.DISPLAYING_MESSAGE:
-            pass
-            
+            pass   
 
     def draw(self):
         if self.state == App.State.GAME:
@@ -39,9 +38,11 @@ class App:
             self.draw_message()
 
     def draw_game(self):
+        pyxel.cls(0)
+        pyxel.camera()
         # backdrop
         pyxel.bltm(0, 30, 0, 0, 30, SCREEN_WIDTH, 78)
-            # # machine overlay
+        # # machine overlay
         pyxel.bltm(0, 0, 0, 0, 0, SCREEN_WIDTH, 30)
         self.draw_background_messages()
         self.claw.draw(True)
@@ -49,18 +50,40 @@ class App:
 
     def draw_message(self):
         pyxel.bltm(0,0,1,0,0,SCREEN_WIDTH,SCREEN_HEIGHT)
+        # pyxel.text(4, 16, f"{self.message.message}", 0, )
+        self.message.update()
 
-    def draw_background_messages(self):
-        def draw_scroll(x, height, col):
-            floor = 108
-            pyxel.rect(x, floor-height, 2, height, col)
-        counter = 0
-        # while counter < SCREEN_WIDTH
-        #   draw a rolled up message at the counter X position at the bottom of the prize machine
-        #   add a random number to the counter
-        draw_scroll(6, 10,13)
+   
+    def randomize_background_messages(self, x, y, rotation, size):
+        random.seed(3)
+        for i in range(0,SCREEN_WIDTH, 6):
+            angle = random.randint(-15,rotation)
+            scale = random.random()*size
+            pyxel.blt(i,100, 0, x , y, 8,8,14, scale=scale+0.9, rotate= angle)
 
-        pass
+    def draw_background_messages(self): 
+        self.randomize_background_messages(0,72, 45, 1.4)
+        self.randomize_background_messages(8,64, 25, 1.0)
+        self.randomize_background_messages(0,64, -15, 1.9)
+        
+
+class Message:
+    def __init__(self):
+        self.messages  = [
+            #                    #
+            "Believe in yourself!",
+            "You can achieve\nanything!",
+            "Stay positive, work\nhard, make it happen!",
+            "Dream big and dare\nto fail!",
+            "The only limit is\nyour mind."
+            ]
+
+        self.message = self.messages[random.randint(0,4)]
+
+    def update(self):
+
+        pyxel.text(20, 24, f"{self.message}", 0, )
+            
 
 class Claw:
     class State(Enum):
@@ -72,18 +95,13 @@ class Claw:
     def __init__(self):
         self.x = 64
         self.y = 20
-
         self.arm_length_min=8
         self.arm_length_max=60 
-
         self.arm_length = self.arm_length_min
         self.state = self.State.DEFAULT
-
         self.grab_timer = 0
     
-
     def draw(self, debug = False):
-
         self.draw_mount()
 
         # draw wheel
@@ -96,45 +114,36 @@ class Claw:
 
         if(debug):
             pyxel.text(4, 4, f"X: {self.x} Y: {self.y}", 0)
-
             # pyxel.line(0, self.y, 140, self.y, 3)
             # pyxel.line(self.x, 0, self.x, 120, 7)
-            
             pyxel.text(4, 10, f"State: {self.state}", 0)
             pyxel.text(4, 16, f"Arm Length: {self.arm_length}", 0)
-
     
     def draw_mount(self):
         pyxel.blt(self.x - 8, 24, 0, 48, 0, 16, 8, 14)
 
     def draw_arm(self):
-        start_y = self.y + 12
-        # drawLine_y(start_y)
-        end_y =  self.arm_length + start_y
-        # drawLine_y(end_y,9)
+        arm_y = self.y + 12
         arm_x=self.x -8
-        arm_y=self.arm_length-8
-        
-        # pyxel.blt(arm_x, arm_y,0, 48, 8,16,8, 14)   
+
         if self.state == Claw.State.DEFAULT:
          pyxel.blt(self.x-8, 32,0,48,8, 16, 8, 14)
         elif self.state == Claw.State.DESCENDING or Claw.State.GRABBING:
             for i in range (self.arm_length):
-             pyxel.blt(self.x-8, i + start_y - ( i % 8) ,0,48,8, 16, 8, 14)
-              
+             pyxel.blt(arm_x, i + arm_y - ( i % 8) ,0,48,8, 16, 8, 14)
         elif self.state == Claw.State.ASCENDING:
             for i in range (self.arm_length):
-                pyxel.blt(self.x-8, i + start_y -( i % 8) , 0, 48, 8, 16, 8, 14)
+                pyxel.blt(arm_x, i + arm_y -( i % 8) , 0, 48, 8, 16, 8, 14)
   
     def draw_claw(self):
         claw_y = self.y +12
+
         if self.state == Claw.State.GRABBING or self.state == Claw.State.ASCENDING:    
             pyxel.blt(self.x-8, claw_y + self.arm_length,0,32,32, 16, 24, 14)
         else:
             pyxel.blt(self.x-8, claw_y + self.arm_length,0,32,16, 16, 16, 14)
 
     def update(self):  
-
         if self.state == Claw.State.DEFAULT:
             self.update_default()
         elif self.state == Claw.State.DESCENDING:
@@ -153,7 +162,7 @@ class Claw:
         if pyxel.btn(pyxel.KEY_LEFT):
             self.x = max(self.x - 2, 0)
         if pyxel.btn(pyxel.KEY_RIGHT):
-            self.x = min(self.x + 2, pyxel.width - 16)   
+            self.x = min(self.x + 2, pyxel.width - 16)  
         if pyxel.btn(pyxel.KEY_SPACE):
             self.state = Claw.State.DESCENDING   
 
